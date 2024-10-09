ac9d0953375554084177938405b0b33782254274
pepetgoodboy <iqbaltw127@gmail.com>
2024-10-09 09:16:10 +0700

    fix input add user

--- src/pages/admin/user/index.jsx
+++ src/pages/admin/user/index.jsx
@@ -112,6 +112,7 @@ export default function User() {
                 id="npm"
                 name="npm"
                 value={npm}
+                required
                 onChange={(e) => setNpm(e.target.value)}
                 className="w-full md:w-auto px-4 py-2 border border-gray-200 outline-none rounded-lg focus:border-[#453bcf] focus:ring-1 focus:ring-[#453bcf]"
               />
